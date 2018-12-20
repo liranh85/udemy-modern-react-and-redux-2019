04ea6b49252002293ab708b17b96774d1e765d0e
Liran Harary <liranh85@gmail.com>
2018-12-20 20:47:20 +0000

    Got to lecture 263

--- streams/client/src/components/streams/StreamCreate.js
+++ streams/client/src/components/streams/StreamCreate.js
@@ -1,79 +1,21 @@
 import React from 'react'
 import { connect } from 'react-redux'
-import { Field, reduxForm } from 'redux-form'
 import { createStream } from '../../actions'
+import StreamForm from './StreamForm'
 
 class StreamCreate extends React.Component {
-  renderError({ error, touched }) {
-    if (touched && error) {
-      return (
-        <div className="ui error message">
-          <div className="header">{error}</div>
-        </div>
-      )
-    }
-  }
-
-  renderInput = ({ input, label, meta }) => {
-    return (
-      <div className={`field ${meta.error && meta.touched ? 'error' : ''}`}>
-        <label>{label}</label>
-        {/* Switching off autocomplete just for this exercise, so we can see the error better */}
-        <input {...input} autoComplete="off" />
-        {this.renderError(meta)}
-      </div>
-    )
-  }
-
   onSubmit = formValues => {
     this.props.createStream(formValues)
   }
 
   render() {
     return (
-      // We need to add the className `error` to the form, because otherwise semantic-ui hides elements with the className `error message`
-      <form
-        className="ui form error"
-        onSubmit={this.props.handleSubmit(this.onSubmit)}
-      >
-        <Field
-          name="title"
-          component={this.renderInput}
-          // custom prop, so will be just passed as argument to our component
-          label="Enter title"
-        />
-        <Field
-          name="description"
-          component={this.renderInput}
-          label="Enter description"
-        />
-        <button className="ui button primary">Submit</button>
-      </form>
+      <div>
+        <h3>Create a Stream</h3>
+        <StreamForm onSubmit={this.onSubmit} />
+      </div>
     )
   }
 }
 
-// Returns an errors object with the error message for any form fields that didn't pass validation
-// When passed to ReduxForm, this function will be called:
-//  - When the form is initially rendered
-//  - Any time the user interacts with it
-const validate = (formValues) => {
-  const errors = {}
-
-  if (!formValues.title) {
-    errors.title = 'You must enter a title'
-  }
-
-  if (!formValues.description) {
-    errors.description = 'You must enter a description'
-  }
-
-  return errors
-}
-
-const formWrapped = reduxForm({
-  form: 'streamCreate',
-  validate
-})(StreamCreate)
-
-export default connect(null, { createStream })(formWrapped)
+export default connect(null, { createStream })(StreamCreate)
--- streams/client/src/components/streams/StreamEdit.js
+++ streams/client/src/components/streams/StreamEdit.js
@@ -1,9 +1,22 @@
 import React from 'react'
+import { connect } from 'react-redux'
+import { fetchStream } from '../../actions'
 
-const StreamEdit = () => {
-  return (
-    <div>StreamEdit</div>
-  )
+class StreamEdit extends React.Component {
+  componentDidMount() {
+    this.props.fetchStream(this.props.match.params.id)
+  }
+
+  render() {
+    console.log(this.props)
+    return (
+      <div>StreamEdit</div>
+    )
+  }
 }
 
-export default StreamEdit
+const mapStateToProps = (state, ownProps) => ({
+  stream: state.streams[ownProps.match.params.id]
+})
+
+export default connect(mapStateToProps, { fetchStream })(StreamEdit)
