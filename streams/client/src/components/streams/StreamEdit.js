6cdfd716b6c8204a0dd5535cf27f2dbaf5746e7f
Liran Harary <liranh85@gmail.com>
2018-12-21 15:37:00 +0000

    Completed Section 20

--- streams/client/src/components/streams/StreamEdit.js
+++ streams/client/src/components/streams/StreamEdit.js
@@ -1,16 +1,32 @@
 import React from 'react'
 import { connect } from 'react-redux'
-import { fetchStream } from '../../actions'
+import _ from 'lodash'
+import { fetchStream, editStream } from '../../actions'
+import StreamForm from './StreamForm'
 
 class StreamEdit extends React.Component {
   componentDidMount() {
     this.props.fetchStream(this.props.match.params.id)
   }
 
+  onSubmit = formValues => {
+    this.props.editStream(this.props.match.params.id, formValues)
+  }
+
   render() {
-    console.log(this.props)
+    if (!this.props.stream) {
+      return <div>Loading...</div>
+    }
+
     return (
-      <div>StreamEdit</div>
+      <div>
+        <h3>Edit a Stream</h3>
+        <StreamForm
+          onSubmit={this.onSubmit}
+          // initialValues is an optional prop (of type object, e.g. { someFieldName: 'Some title', ... }) expected by ReduxForm, which allows us to set the initial values for any of our ReduxForm fields. Since StreamForm is wrapped by ReduxForm, we are essentially passing this prop (initialValues) to the ReduxForm component
+          initialValues={_.pick(this.props.stream, 'title', 'description')}
+        />
+      </div>
     )
   }
 }
@@ -19,4 +35,4 @@ const mapStateToProps = (state, ownProps) => ({
   stream: state.streams[ownProps.match.params.id]
 })
 
-export default connect(mapStateToProps, { fetchStream })(StreamEdit)
+export default connect(mapStateToProps, { fetchStream, editStream })(StreamEdit)
