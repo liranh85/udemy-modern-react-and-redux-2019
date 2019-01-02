8548aba0850a8969ab358703777521d16e5e1d69
Liran Harary <liranh85@gmail.com>
2019-01-02 17:35:19 +0000

    Code reuse example. Section 25 complete

--- hooks-simple/src/components/App.js
+++ hooks-simple/src/components/App.js
@@ -1,9 +1,10 @@
 import React, { useState } from 'react'
 import ResourceList from './ResourceList'
+import UserList from './UserList'
 
 const App = () => {
   // `useState` takes the initial state and returns an array with the current value (which we called `resource` and is analagous to this.state) and a method to change it (which we called `setResource` and is analagous to this.setState)
-  // Calling the method we named `setResource` causes the component to re-render, like setState
+  // Calling the method we named `setResource` causes the component to re-render, like this.setState
   // Array destructuring
   const [resource, setResource] = useState('posts')
   // The above is basically:
@@ -11,6 +12,7 @@ const App = () => {
 
   return (
     <div>
+      <UserList />
       <div>
         <button onClick={() => setResource('posts')}>Posts</button>
         <button onClick={() => setResource('todos')}>Todos</button>
