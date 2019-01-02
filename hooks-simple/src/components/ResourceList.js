7ce18af075df382f33c2f76376deb6ebefb922fc
Liran Harary <liranh85@gmail.com>
2019-01-02 17:19:44 +0000

    Extract useResources function to a dedicated reusable component

--- hooks-simple/src/components/ResourceList.js
+++ hooks-simple/src/components/ResourceList.js
@@ -1,22 +1,5 @@
-import React, { useState, useEffect } from 'react'
-import axios from 'axios'
-
-const useResources = resource => {
-  const [resources, setResources] = useState([])
-
-  // Analogous to a combination of (componentDidMount + componentDidUpdate) in a class-based component
-  // Calls the callback function if the values in the second argument (the array) have changed
-  // Leave array empty if you want the callback function to be called only once (like componentDidMount)
-  // Don't pass the array at all if you want the callback function to be called on every update (additionally to on component mount), regardless of the difference between prev and current propd
-  useEffect(() => {
-    (async resource => {
-      const response = await axios.get(`https://jsonplaceholder.typicode.com/${resource}`)
-      setResources(response.data)
-    })(resource)
-  }, [resource])
-
-  return resources
-}
+import React from 'react'
+import useResources from './useResources'
 
 const ResourceList = ({ resource }) => {
   const resources = useResources(resource)
