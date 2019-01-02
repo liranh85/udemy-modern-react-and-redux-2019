224cdf6376e010fd178480c7ae3e3a1d18f04795
Liran Harary <liranh85@gmail.com>
2019-01-02 17:00:43 +0000

    Render actual resources rather than just lengh

--- hooks-simple/src/components/ResourceList.js
+++ hooks-simple/src/components/ResourceList.js
@@ -18,7 +18,11 @@ const ResourceList = ({ resource }) => {
   }, [resource])
 
   return (
-    <div>{resources.length}</div>
+    <ul>
+      {resources.map(record => (
+        <li key={record.id}>{record.title}</li>
+      ))}
+    </ul>
   )
 }
 
