9833122a294d1d580fc8d266ac2de7e8aa4f58fe
Liran Harary <liranh85@gmail.com>
2019-01-02 17:05:47 +0000

    Move logic in named function fetchResource() to an IIFE directly in useEffect()s callback function

--- hooks-simple/src/components/ResourceList.js
+++ hooks-simple/src/components/ResourceList.js
@@ -4,17 +4,15 @@ import axios from 'axios'
 const ResourceList = ({ resource }) => {
   const [resources, setResources] = useState([])
 
-  const fetchResource = async resource => {
-    const response = await axios.get(`https://jsonplaceholder.typicode.com/${resource}`)
-    setResources(response.data)
-  }
-
   // Analogous to a combination of (componentDidMount + componentDidUpdate) in a class-based component
   // Calls the callback function if the values in the second argument (the array) have changed
   // Leave array empty if you want the callback function to be called only once (like componentDidMount)
   // Don't pass the array at all if you want the callback function to be called on every update (additionally to on component mount), regardless of the difference between prev and current propd
   useEffect(() => {
-    fetchResource(resource)
+    (async resource => {
+      const response = await axios.get(`https://jsonplaceholder.typicode.com/${resource}`)
+      setResources(response.data)
+    })(resource)
   }, [resource])
 
   return (
